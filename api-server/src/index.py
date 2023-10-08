4f8de2ae6baf66ffda28baa0149fb836b9d0bcd3
rongrongtu <takuyousou@gmail.com>
2023-10-08 13:16:13 +0900

    [add] issue/#39 [Users-002] パスワード変更API作成

--- api-server/src/index.py
+++ api-server/src/index.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify
 
 from models.http_codes import Response
 from api_methods.user_register import register_user
+from api_methods.user_changepwd import changepwd_user
 
 # .envファイルを読込み
 from dotenv import load_dotenv
@@ -17,6 +18,13 @@ app = Flask(__name__)
 def route_users_post():
     body: dict = request.get_json(force=True)
     res: Response = register_user(body)
+    return jsonify(res.body), res.code
+
+# [Users-002] ユーザーパスワード変更
+@app.route('/api/users', methods=["PUT"])
+def route_users_put():
+    body: dict = request.get_json(force=True)
+    res: Response = changepwd_user(body)
     return jsonify(res.body), res.code
 
 # Server Test
