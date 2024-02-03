88381494feb1cbba763eeab6daf7e4ee3ae488b2
rongrongtu <takuyousou@gmail.com>
2024-02-03 22:05:15 +0900

    [add] issue/#63 データベース設計を直し

--- Docker/mysql/sql/init.sql
+++ Docker/mysql/sql/init.sql
@@ -15,9 +15,9 @@ comment = 'ユーザ情報';
 
 CREATE TABLE craft_location (
   craft_id int(10) unsigned not null auto_increment comment '工芸品　ID',
-  craft_name varchar(255) not null comment '工芸品　名前',
-  craft_japanese_name varchar(255) comment '工芸品　日本語名前',
-  craft_url varchar(255) comment '工芸品　説明文URL',
+  name varchar(255) not null comment '工芸品　名前',
+  japanese_name varchar(255) comment '工芸品　日本語名前',
+  url varchar(255) comment '工芸品　説明文URL',
   created_at timestamp not null default current_timestamp comment '工芸品　作成時刻', 
   updated_at timestamp not null default current_timestamp on update current_timestamp comment '工芸品　更新時刻',
   is_deleted boolean not null default false comment '工芸品　削除フラグ',
@@ -26,8 +26,9 @@ CREATE TABLE craft_location (
 comment = '工芸品情報';
 
 CREATE TABLE craft_characteristic (
-  craft_id int(10) unsigned not null auto_increment comment '工芸品　ID',
-  craft_description varchar(255) comment '特徴語　説明文',
+  id int(10) unsigned not null auto_increment comment 'ID',
+  craft_id int(10) unsigned not null comment '工芸品　ID',
+  description TEXT comment '特徴語　説明文',
   region varchar(255) comment '特徴語　地域',
   category varchar(255) comment '特徴語　品類',
   material varchar(255) comment '特徴語　材質',
@@ -37,7 +38,7 @@ CREATE TABLE craft_characteristic (
   created_at timestamp not null default current_timestamp comment '特徴語　作成時刻', 
   updated_at timestamp not null default current_timestamp on update current_timestamp comment '特徴語　更新時刻',
   is_deleted boolean not null default false comment '特徴語　削除フラグ',
-  PRIMARY KEY (craft_id),
+  PRIMARY KEY (id),
   FOREIGN KEY(craft_id) REFERENCES craft_location(craft_id)
 )
 comment = '工芸品特徴語';
